2c874c9e61284938313bc2e7bafaa8ad71eefac7
Camella <caixiasan@qq.com>
2017-07-04 16:27:38 +0800

    new app

---
+++ comliled.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var number = [1, 2, 3];
+var doubleNumbers = numbers.map(function (number) {
+  return number * 2;
+});
+console.log(doubleNumbers);
+
+var itme = 0;
+input.map(function (item) {
+  return item + 1;
+});
---
+++ object.js
@@ -0,0 +1,5 @@
+let mike={name:"mike","age":'40'}
+
+mike={...mike,sex:male}
+
+console.log(mike);
